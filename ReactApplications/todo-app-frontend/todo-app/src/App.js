bab72addd8e3d2e3f4deecfc55b79132c341e3c5
Habibur Rahman <habibur0199mhr@gmail.com>
2024-08-28 00:09:34 +0600

    initial commit

--- ReactApplications/todo-app-frontend/todo-app/src/App.js
+++ ReactApplications/todo-app-frontend/todo-app/src/App.js
@@ -1,45 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import { Component } from 'react'
-import FirstComponent from './components/learning-examples/FirstComponent'
-import LearningJavaScript from './components/learning-examples/LearningJavaScript';
+import Counter from './components/counter/Counter';
 
 function App() {
   return (
     <div className="App">
       My Todo application updated
-      <FirstComponent></FirstComponent>
-      <MySecondComponent></MySecondComponent>
-      <ThirdComponent />
-      <ThirForthonent></ThirForthonent>
-      <LearningJavaScript></LearningJavaScript>
+      <Counter></Counter>
     </div>
   );
-}
-
-function MySecondComponent() {
-  return (
-    <div className="MySecondComponent">MySecondComponent</div>
-  );
-}
-
-class ThirdComponent extends Component {
-  render() {
-    return (
-      <div className="ThirdComponent">Third Component</div>
-    );
-  }
-}
-
-class ThirForthonent extends Component {
-  render() {
-    return (
-      <>
-      <div className="ForthComponent">Forth Component</div>
-      <div className="ForthComponent">Forth Component</div>
-      </>
-    );
-  }
 }
 
 export default App;
